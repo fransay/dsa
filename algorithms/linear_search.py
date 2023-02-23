2f91dba125a0313f25fa6680a10f8ad6aef0db37
Francis Osei Annin <francisannin5@gmail.com>
2023-02-23 16:09:53 +0000

    Added docs and edge cases

--- algorithms/linear_search.py
+++ algorithms/linear_search.py
@@ -9,20 +9,47 @@ as an array/list.
 
 
 """
-TIME AND SPACE COMPLEXITY
--------------------------
-Big OH: O(n)
+Algorithm Explained
+
+* Given an array or a collection data structure
+* The algorithm walks through each item in the structure
+* it checks each element and compares it to the desired/target element.
+* if found, it breaks and return a boolean 
+* with the position of the element in the array
+
+EDGE CASES:
+This naive implementation doesn't really work well for all data types.
+For example, a floating point number comparism of (0.5 + 0.1 != 0.6),
+this is as a result of internal rounding error. In such an instance, the
+algorithm designed below won't be effective and efficient.
+We must take care of this edge case.
+... see function **linear_search_float**
 
 """
 def linear_search(array, target_element):
     """
     This function accepts two parameters, array and target_element arguments
     returns a boolean and position(index) of the target element in the array/list.
+    Big Oh: O(N)
     """
     for index, element in enumerate(array):
         if element == target_element:
             return True, index
     return False, None
+
+
+def linear_search_float(array, target_element:float):
+    """
+    **edge case**
+    this implementation handles the edge case described on line 20 - 26
+    """
+    for index, element in enumerate(array):
+        if abs(element-target_element) <1e-9: # determines the absolute difference between the 
+            # target element and the element in array. if the difference is less than 
+            # 1e-9 which is a very small number, then we can safely say,they are equal
+            return True, index
+        return False, None 
+
 
 
 if __name__ == '__main__':
