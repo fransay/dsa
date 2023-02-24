3a5f9035ed27690547b59193f20d31de5ad19b62
Francis Osei Annin <francisannin5@gmail.com>
2023-02-24 22:47:38 +0000

    Added new script file

---
+++ algorithms/fundamentals/binary_search.py
@@ -0,0 +1,6 @@
+"""
+AUTHOR: Francis Osei Annin
+DATE: 23/02/23
+DESCRIPTION: This program searches for an element in a sequential data structure such 
+an array/list using the binary search algorithm.
+"""
