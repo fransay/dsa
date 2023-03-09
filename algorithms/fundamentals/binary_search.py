f779f791df97893a59122079362ab105d7a3b592
Francis Osei Annin <francisannin5@gmail.com>
2023-03-09 10:06:06 +0000

    Refactored comments

--- algorithms/fundamentals/binary_search.py
+++ algorithms/fundamentals/binary_search.py
@@ -5,15 +5,15 @@ DESCRIPTION: This program searches for an element in a sequential data structure
 an array/list using the binary search algorithm.
 """
 
-
 """
-ALGORITHMS EXPALINED
+ALGORITHMS EXPLAINED
 * Given an array of n size and a target which represents a search element.
 * A loop is set with a condition which breaks when the index of the first element
 * is greater than or equal to the index of the last element.
 * At each iteration, the array search space is divided into two with a if condition
 * the algorithm returns a boolean and index value of the search result.
 """
+
 
 def binary_search(array, target):
     """
@@ -23,7 +23,7 @@ def binary_search(array, target):
     """
     low_index, high_index = 0, len(array) - 1
     while low_index <= high_index:
-        mid_index = low_index + (high_index - low_index)//2
+        mid_index = low_index + (high_index - low_index) // 2
         if target == array[mid_index]:
             return True, mid_index
         elif target < array[mid_index]:
@@ -32,11 +32,11 @@ def binary_search(array, target):
             low_index = mid_index + 1
     return False, None
 
+
 def main():
-    print(binary_search([1,2,3,4,5,7],7))
-    print(binary_search([7,8,9,10,11,12],10.00))
+    print(binary_search([1, 2, 3, 4, 5, 7], 7))
+    print(binary_search([7, 8, 9, 10, 11, 12], 10.00))
+
 
 if __name__ == '__main__':
     main()
-    
-
