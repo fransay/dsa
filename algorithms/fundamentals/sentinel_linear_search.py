b2b61b545e80a0e4e5ddb1f49b0208d5832a2646
Francis Osei Annin <francisannin5@gmail.com>
2023-03-17 10:23:06 +0000

    Included sentinel_ls(): not populated.

---
+++ algorithms/fundamentals/sentinel_linear_search.py
@@ -0,0 +1,14 @@
+# AUTHOR: Francis Osei Annin
+# DATE: 17/02/23
+# DESCRIPTION: This program linearly searches for desired elements in a collection data structure,
+# using the sentinel approach.
+
+# sentinel_ls: sentinel_ls is an abbreviation of the term sentinel linear search
+def sentinel_ls():
+    """
+    """
+    pass
+
+
+if __name__ == '__main__':
+    print(sentinel_ls())
