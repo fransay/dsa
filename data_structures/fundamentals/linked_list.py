372dfe35df62967ffc00d5fa44f695a8ff0f897f
Francis Osei Annin <francisannin5@gmail.com>
2023-03-18 19:07:18 +0000

    Added linked_list.py

---
+++ data_structures/fundamentals/linked_list.py
@@ -0,0 +1,9 @@
+# AUTHOR: Francis Osei Annin
+# DATE: 18/03/23
+# DESCRIPTION: This program creates a type for linkedlist with relevant methods.
+
+
+# single linked list
+# double linked list
+# circular linked list
+
