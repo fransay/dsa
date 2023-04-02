bf895bfafed7ef9b9f39d589de91654dc3c35d83
Francis Osei Annin <francisannin5@gmail.com>
2023-04-02 22:28:48 +0000

    Merge sort implementation included

---
+++ algorithms/fundamentals/sort/merge_sort.py
@@ -0,0 +1,22 @@
+# AUTHOR: Francis Osei Annin
+# DATE: 02/04/23
+# DESCRIPTION: This program using the merge sort algorithm to rank elements in a sequential data structure
+
+"""
+ALGORITHM EXPLAINED
+"""
+
+
+def merge_sort():
+    """
+    returns a sorted collection of elements
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
