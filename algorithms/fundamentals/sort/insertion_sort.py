574314059443eb86b3a9b4a1e185194e0fdc052d
Francis Osei Annin <francisannin5@gmail.com>
2023-04-11 20:21:37 +0000

    Insertion sort functions defined

---
+++ algorithms/fundamentals/sort/insertion_sort.py
@@ -0,0 +1,20 @@
+# AUTHOR: Francis Osei Annin
+# DATE: 11/04/23
+# DESCRIPTION: A program that sort elements in a collection based using the insertion sort method
+
+"""
+ALGORITHM EXPLAINED
+
+"""
+
+
+def insertion_sort(array: list):
+    """
+    accepts an unsorted array as input,
+    returns a sorted array using the conventional insertion sort algorithm.
+    """
+    return None
+
+
+if __name__ == '__main__':
+    print(insertion_sort([1, 31, 4]))
