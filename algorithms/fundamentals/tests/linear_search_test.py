77ac44d5876f1f199729afb1458542f9d537b721
Francis Osei Annin <francisannin5@gmail.com>
2024-01-16 23:33:59 +0000

    test: test failed

--- algorithms/fundamentals/tests/linear_search_test.py
+++ algorithms/fundamentals/tests/linear_search_test.py
@@ -1,37 +1,21 @@
-# Author: Francis Osei Annin
-# Date: 01/10/23
-# Description: Linear Search Unit Tests
-
 import unittest
-import algorithms.fundamentals.search.linear_search as ls
+from ..search.linear_search import LinearSearch
 
 
-class LinearSearchTest(unittest.TestCase):
-    """run all linear search unit tests here"""
+class TestBinarySearch(unittest.TestCase):
+    """Test Binary Search"""
 
-    def test_linear_search(self):
-        program_outcome_test_one = ls.linear_search([1, 2, 3, 4, 5, 6, 7], 2)
-        expected_outcome_test_one = (True, 1)
-        self.assertEqual(program_outcome_test_one, expected_outcome_test_one)
+    def test_find(self):
+        """test find method"""
+        test_array = [1, 2, 3, 4]
+        lin_search = LinearSearch(test_array)
 
-        program_outcome_test_two = ls.linear_search(["male", "female"], "male")
-        expected_outcome_test_two = (True, 0)
-        self.assertEqual(program_outcome_test_two, expected_outcome_test_two)
+        result1 = lin_search.find(0)  # expect false
+        expected1 = False
 
-        program_outcome_test_three = ls.linear_search(["male", "female"], "female")
-        expected_outcome_test_three = (True, 1)
-        self.assertEqual(program_outcome_test_three, expected_outcome_test_three)
+        result2 = lin_search.find(1)  # expect true
+        expected2 = True
 
-        program_outcome_test_four = ls.linear_search(["male", "female"], "mango")
-        expected_outcome_test_four = (False, None)
-        self.assertEqual(program_outcome_test_four, expected_outcome_test_four)
+        self.assertEqual(result1, expected1)
+        self.assertEqual(result2, expected2)
 
-        program_outcome_test_five = ls.linear_search([1, 2, 3, 4, 5, 65, 10, 7, 8, 6], 10)
-        expected_outcome_test_five = (True, 6)
-        self.assertEqual(program_outcome_test_five, expected_outcome_test_five)
-
-    def test_linear_search_float(self):
-        pass
-
-    def linear_search_upgrade(self):
-        pass
